87172dc27cb32309d5c640c5644be8ce20d5b32b
Atsushi Yamazaki <yamazaki@AtsushiYamazakinoMacBook-Pro.local>
2017-12-12 23:50:10 +0900

    add home

---
+++ home.py
@@ -0,0 +1,105 @@
+""" home.py - Fabricate.IO
+
+    This is a demo python file showing what can be done with the debounce_handler.
+    The handler prints True when you say "Alexa, device on" and False when you say
+    "Alexa, device off".
+
+    If you have two or more Echos, it only handles the one that hears you more clearly.
+    You can have an Echo per room and not worry about your handlers triggering for
+    those other rooms.
+
+    The IP of the triggering Echo is also passed into the act() function, so you can
+    do different things based on which Echo triggered the handler.
+"""
+
+import fauxmo
+import logging
+import time
+import json
+import serial
+import os
+
+from debounce_handler import debounce_handler
+
+logging.basicConfig(level=logging.DEBUG)
+ir_serial = serial.Serial("/dev/ttyACM0", 9600, timeout = 1)
+
+class device_handler(debounce_handler):
+    """Publishes the on/off state requested,
+       and the IP address of the Echo making the request.
+    """
+    TRIGGERS = {"lights": 52000, "tv": 52001, "air-condition": 52002}
+
+    def act(self, client_address, state, name):
+        print "State", state, "on ", name,  "from client @", client_address
+        if name == "lights":
+            self.playLights(state)
+        return True
+
+    def playIR(self, path):
+        if path and os.path.isfile(path):
+            logging.debug("Playing IR with %s ..." % path)
+            f = open(path)
+            data = json.load(f)
+            f.close()
+            recNumber = len(data['data'])
+            rawX = data['data']
+
+            ir_serial.write("n,%d\r\n" % recNumber)
+            ir_serial.readline()
+
+            postScale = data['postscale']
+            ir_serial.write("k,%d\r\n" % postScale)
+            #time.sleep(1.0)
+            msg = ir_serial.readline()
+            #print msg
+
+            for n in range(recNumber):
+                bank = n / 64
+                pos = n % 64
+                if (pos == 0):
+                    ir_serial.write("b,%d\r\n" % bank)
+
+                ir_serial.write("w,%d,%d\n\r" % (pos, rawX[n]))
+
+            ir_serial.write("p\r\n")
+            msg = ir_serial.readline()
+            logging.debug(msg)
+            #ir_serial.close()
+        else:
+            logging.debug("Playing IR...")
+            ir_serial.write("p\r\n")
+            time.sleep(1.0)
+            msg = ir_serial.readline()
+            logging.debug(msg)
+
+    def playLights(self, state):
+        if state == True:
+            self.playIR("/usr/local/src/irmcli/light_on.json")
+        else:
+            self.playIR("/usr/local/src/irmcli/light_off.json")
+        return True
+
+if __name__ == "__main__":
+    # Startup the fauxmo server
+    fauxmo.DEBUG = True
+    p = fauxmo.poller()
+    u = fauxmo.upnp_broadcast_responder()
+    u.init_socket()
+    p.add(u)
+
+    # Register the device callback as a fauxmo handler
+    d = device_handler()
+    for trig, port in d.TRIGGERS.items():
+        fauxmo.fauxmo(trig, u, p, None, port, d)
+
+    # Loop and poll for incoming Echo requests
+    logging.debug("Entering fauxmo polling loop")
+    while True:
+        try:
+            # Allow time for a ctrl-c to stop the process
+            p.poll(100)
+            time.sleep(0.1)
+        except Exception, e:
+            logging.critical("Critical exception: " + str(e))
+            break
